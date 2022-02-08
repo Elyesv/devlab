9b41d086c6e9256a85d4a31b7d6053313335357a
Elyes Voisin <elyes.voisin@edu.devinci.fr>
2022-02-08 12:51:59 +0100

    :lipstick: ADD footer + responsive

--- functions.php
+++ functions.php
@@ -187,8 +187,33 @@ if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
 
+
+if( function_exists('acf_add_options_page') ) {
+
+    acf_add_options_page(array(
+        'page_title' 	=> 'Theme General Settings',
+        'menu_title'	=> 'Theme Settings',
+        'menu_slug' 	=> 'theme-general-settings',
+        'capability'	=> 'edit_posts',
+        'redirect'		=> false
+    ));
+
+    acf_add_options_sub_page(array(
+        'page_title' 	=> 'Theme Header Settings',
+        'menu_title'	=> 'Header',
+        'parent_slug'	=> 'theme-general-settings',
+    ));
+
+    acf_add_options_sub_page(array(
+        'page_title' 	=> 'Theme Footer Settings',
+        'menu_title'	=> 'Footer',
+        'parent_slug'	=> 'theme-general-settings',
+    ));
+
+}
+
 add_filter('wp_nav_menu_objects', function($items) {
-    foreach ($items as $item) {
+    foreach ($items as &$item) {
         $icon = get_field('icon', $item);
 
         if ($icon) {
