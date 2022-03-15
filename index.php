bfed2ba5e437b6b519fe639660bbfc96afd877d0
PALVADEAU  Florian <florian.palvadeau@edu.devinci.fr>
2022-03-15 13:07:36 +0100

    ADD index page, responsive missing

--- index.php
+++ index.php
@@ -15,43 +15,46 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<?php
-		if ( have_posts() ) :
-
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
-
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
-
-				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_type() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
+	<main class="site-main-index">
+		<p>Pour l'amour des goodies</p>
 	</main><!-- #main -->
 
+	<div class="site-secondary-index">
+		<div class="container">
+			<div class="leftPart">
+				<div class="left">
+					<div class="textArea">
+						<p class="p1">Coup de coeur</p>
+						<p class="p2">IIMPACT</p>
+						<p class="p3">En voir plus →</p>
+					</div>
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71pWzhdJNwL._AC_UL640_QL65_ML3_.png"  alt="Article image">
+					</div>
+				</div>
+				<div class="image">
+					<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.png" class="mainImg" alt="Main article image"> 
+				</div>	
+			</div>
+			
+			<div class="rightPart">
+				<div class="topContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/61sbMiUnoGL._AC_UL640_QL65_ML3_.png" alt="Article image">
+					</div>
+					<div class="image2">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71YXzeOuslL._AC_UY879_.png" alt="Article image">
+					</div>
+				</div>
+				<div class="bottomContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/81fPKd-2AYL._AC_SL1500_.png" alt="Article image">
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales ex lorem, non congue augue mattis vel. Aenean ornare scelerisque suscipit. Cras aliquam vestibulum pharetra</p>
+				</div>
+			</div>
+		</div>
+	</div><!-- #main -->
+
 <?php
-get_sidebar();
 get_footer();
