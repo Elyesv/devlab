6a25552ee042bcc2e45a14da6c36468f4360c426
PALVADEAU  Florian <florian.palvadeau@edu.devinci.fr>
2022-04-05 10:50:23 +0200

    ADD index responsive

--- index.php
+++ index.php
@@ -56,5 +56,116 @@ get_header();
 		</div>
 	</div><!-- #main -->
 
+	<div class="site-secondary-index">
+		<div class="container">
+			<div class="leftPart">
+				<div class="left">
+					<div class="textArea">
+						<p class="p1">Coup de coeur</p>
+						<p class="p2">IIMPACT</p>
+						<p class="p3">En voir plus →</p>
+					</div>
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71pWzhdJNwL._AC_UL640_QL65_ML3_.png"  alt="Article image">
+					</div>
+				</div>
+				<div class="image">
+					<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.png" class="mainImg" alt="Main article image"> 
+				</div>	
+			</div>
+			
+			<div class="rightPart">
+				<div class="topContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/61sbMiUnoGL._AC_UL640_QL65_ML3_.png" alt="Article image">
+					</div>
+					<div class="image2">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71YXzeOuslL._AC_UY879_.png" alt="Article image">
+					</div>
+				</div>
+				<div class="bottomContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/81fPKd-2AYL._AC_SL1500_.png" alt="Article image">
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales ex lorem, non congue augue mattis vel. Aenean ornare scelerisque suscipit. Cras aliquam vestibulum pharetra</p>
+				</div>
+			</div>
+		</div>
+	</div><!-- #main -->
+
+	<div class="site-secondary-index">
+		<div class="container">
+			<div class="leftPart">
+				<div class="left">
+					<div class="textArea">
+						<p class="p1">Coup de coeur</p>
+						<p class="p2">IIMPACT</p>
+						<p class="p3">En voir plus →</p>
+					</div>
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71pWzhdJNwL._AC_UL640_QL65_ML3_.png"  alt="Article image">
+					</div>
+				</div>
+				<div class="image">
+					<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.png" class="mainImg" alt="Main article image"> 
+				</div>	
+			</div>
+			
+			<div class="rightPart">
+				<div class="topContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/61sbMiUnoGL._AC_UL640_QL65_ML3_.png" alt="Article image">
+					</div>
+					<div class="image2">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71YXzeOuslL._AC_UY879_.png" alt="Article image">
+					</div>
+				</div>
+				<div class="bottomContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/81fPKd-2AYL._AC_SL1500_.png" alt="Article image">
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales ex lorem, non congue augue mattis vel. Aenean ornare scelerisque suscipit. Cras aliquam vestibulum pharetra</p>
+				</div>
+			</div>
+		</div>
+	</div><!-- #main -->
+
+	<div class="site-secondary-index">
+		<div class="container">
+			<div class="leftPart">
+				<div class="left">
+					<div class="textArea">
+						<p class="p1">Coup de coeur</p>
+						<p class="p2">IIMPACT</p>
+						<p class="p3">En voir plus →</p>
+					</div>
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71pWzhdJNwL._AC_UL640_QL65_ML3_.png"  alt="Article image">
+					</div>
+				</div>
+				<div class="image">
+					<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.png" class="mainImg" alt="Main article image"> 
+				</div>	
+			</div>
+			
+			<div class="rightPart">
+				<div class="topContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/61sbMiUnoGL._AC_UL640_QL65_ML3_.png" alt="Article image">
+					</div>
+					<div class="image2">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/71YXzeOuslL._AC_UY879_.png" alt="Article image">
+					</div>
+				</div>
+				<div class="bottomContent">
+					<div class="image">
+						<img src="http://localhost/devlabb/wp-content/uploads/2022/03/81fPKd-2AYL._AC_SL1500_.png" alt="Article image">
+					</div>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sodales ex lorem, non congue augue mattis vel. Aenean ornare scelerisque suscipit. Cras aliquam vestibulum pharetra</p>
+				</div>
+			</div>
+		</div>
+	</div><!-- #main -->
+
 <?php
 get_footer();
