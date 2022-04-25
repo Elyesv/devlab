b4e935b64d83807a3f0d1489db35fe2b9f9b9977
Elyes Voisin <elyes.voisin@edu.devinci.fr>
2022-04-25 12:34:43 +0200

    ADD 404

--- 404.php
+++ 404.php
@@ -10,51 +10,13 @@
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-
-		<section class="error-404 not-found">
-			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'pour_lamoure_des_goodies' ); ?></h1>
-			</header><!-- .page-header -->
-
-			<div class="page-content">
-				<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'pour_lamoure_des_goodies' ); ?></p>
-
-					<?php
-					get_search_form();
-
-					the_widget( 'WP_Widget_Recent_Posts' );
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'pour_lamoure_des_goodies' ); ?></h2>
-						<ul>
-							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
-							?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-					/* translators: %1$s: smiley */
-					$pour_lamoure_des_goodies_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'pour_lamoure_des_goodies' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$pour_lamoure_des_goodies_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-			</div><!-- .page-content -->
-		</section><!-- .error-404 -->
-
-	</main><!-- #main -->
+	<div class="uknown">
+        <h1 class="bigtitle">404</h1>
+        <p class="uknown-title">OOPS, This Page Was Not Found</p>
+        <p class="uknown-subtitle">The link might be corrupted</p>
+        <p class="uknown-subtitle-2">Or the page may have been removed</p>
+        <a href="index.php" class="uknown-btn">Go Back Home</a>
+    </div>
 
 <?php
 get_footer();
