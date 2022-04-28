946f5306610a98a5743f432fc20f2814fefcf5f8
Elyes Voisin <elyes.voisin@edu.devinci.fr>
2022-04-28 10:23:08 +0200

    ADD contact

--- footer.php
+++ footer.php
@@ -56,5 +56,6 @@
             ?>
         </div>
     </footer>
+    <?php wp_footer(); ?>
 </body>
 </html>
--- index.php
+++ index.php
@@ -19,78 +19,68 @@ get_header();
 ?>
 
     <div class="containerIndex">
-        <main class="site-main-index">
-            <p>Pour l'amour des goodies</p>
-        </main><!-- #main -->
+    <main class="site-main-index">
+        <p>Pour l'amour des goodies</p>
+    </main><!-- #main -->
 
-        <?php foreach ($assos as $asso){
+<?php foreach ($assos as $asso){
 
-            $args = array(
-                'post_type' => 'product',
-                'posts_per_page' => 5,
-                'product_cat' => $asso['nom'],
-            );
+    $args = array(
+        'post_type' => 'product',
+        'posts_per_page' => 5,
+        'product_cat' => $asso['nom'],
+    );
 
-            $articles = [];
+    $articles = [];
 
-            $loop = new WP_Query( $args );
-            if ( $loop->have_posts() ) {
-                while ( $loop->have_posts() ) : $loop->the_post();
-                    array_push($articles, get_permalink(), get_the_post_thumbnail_url());
-                endwhile;
-            }
-            wp_reset_postdata();
+    $loop = new WP_Query( $args );
+    if ( $loop->have_posts() ) {
+        while ( $loop->have_posts() ) : $loop->the_post();
+            array_push($articles, get_permalink(), get_the_post_thumbnail_url());
+        endwhile;
+    }
+    wp_reset_postdata();
 
-        ?>
+    ?>
 
-            <div class="site-secondary-index">
-            <div class="container">
-                <div class="leftPart">
-                    <div class="left">
-                        <div class="textArea">
-                            <p class="p1">Coup de coeur</p>
-                            <p class="p2"><?= $asso['nom'] ?></p>
-                            <a href="http://localhost/devlab/product-category/<?= $asso['nom'] ?>" class="p3">En voir plus →</a>
-                        </div>
-                        <a href="<?= $articles[0] ?>">
-                            <div class="image">
-                                <img src="<?= $articles[1] ?>"  alt="Article image">
-                            </div>
-                        </a>
+    <div class="site-secondary-index">
+        <div class="container">
+            <div class="leftPart">
+                <div class="left">
+                    <div class="textArea">
+                        <p class="p1">Coup de coeur</p>
+                        <p class="p2"><?= $asso['nom'] ?></p>
+                        <a href="http://localhost/devlab/product-category/<?= $asso['nom'] ?>" class="p3">En voir plus →</a>
                     </div>
-                    <a href="<?= $articles[2] ?>">
-                        <div class="image">
-                            <img src="<?= $articles[3] ?>" class="mainImg" alt="Main article image">
-                        </div>
+                    <a href="<?= $articles[0] ?>">
+                        <img class="image" src="<?= $articles[1] ?>"  alt="Article image">
                     </a>
                 </div>
+                <a href="<?= $articles[2] ?>">
+                    <img class="image" src="<?= $articles[3] ?>" class="mainImg" alt="Main article image">
+                </a>
+            </div>
 
-                <div class="rightPart">
-                    <div class="topContent">
-                        <a href="<?= $articles[4] ?>">
-                        <div class="image">
-                            <img src="<?= $articles[5] ?>" alt="Article image">
-                        </div>
-                        </a>
-                        <a href="<?= $articles[6] ?>">
-                            <div class="image2">
-                                <img src="<?= $articles[7] ?>" alt="Article image">
-                            </div>
-                        </a>
-                    </div>
-                    <div class="bottomContent">
-                        <a href="<?= $articles[8] ?>">
-                            <div class="image">
-                                <img src="<?= $articles[9] ?>" alt="Article image">
-                            </div>
-                        </a>
-                        <p><?= $asso['description'] ?></p>
-                    </div>
+            <div class="rightPart">
+                <div class="topContent">
+                    <a class="image1" href="<?= $articles[4] ?>">
+                        <img class="image" src="<?= $articles[5] ?>" alt="Article image">
+                    </a>
+                    <a class="image2" href="<?= $articles[6] ?>">
+                        <img src="<?= $articles[7] ?>" alt="Article image">
+                    </a>
+                </div>
+                <div class="bottomContent">
+                    <a href="<?= $articles[8] ?>">
+                        <img class="image" src="<?= $articles[9] ?>" alt="Article image">
+                    </a>
+                    <p><?= $asso['description'] ?></p>
                 </div>
             </div>
-        </div><!-- #main -->
-        <?php }
-        ?>
+        </div>
+    </div><!-- #main -->
+<?php }
+?>
 
 
 <?php
